ec958c6d86a9f6051449014a2dcd29ab084ee4d4
huseyinkrs <121403058+HK27027@users.noreply.github.com>
2025-08-21 17:22:29 +0300

    wfwfaqw

--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e2337b6f5ffb9e8508a4e0aead0ba4043e9dfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158dc620372288dff22cb5da00f5ad19703dac5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
